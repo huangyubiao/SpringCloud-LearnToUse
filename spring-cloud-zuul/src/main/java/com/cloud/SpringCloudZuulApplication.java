b0cc24b5a396a6c7d16e5624c649e65d3648f517
黄玉彪|Yubiao Huang <yubiao.huang@99bill.com>
2019-08-06 09:45:26 +0800

    统一网关

---
+++ spring-cloud-zuul/src/main/java/com/cloud/SpringCloudZuulApplication.java
@@ -0,0 +1,17 @@
+package com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringCloudZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudZuulApplication.class, args);
+    }
+
+}
